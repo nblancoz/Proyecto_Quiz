34d9baba284d3838dc100482f6b53f71b085b931
nblancoz <nblancozappala@gmail.com>
2023-10-19 13:50:05 +0200

    quiz terminado

--- main.js
+++ main.js
@@ -8,19 +8,24 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answerButtons");
 const cardContainer = document.getElementById("cardContainer");
 const resultsContainer = document.getElementById("resultsContainer");
-const answerComprobation = document.getElementById("answerComprobation")
-const incorrectAnswer = document.getElementById("incorrectAnswer")
-const correctAnswer = document.getElementById("correctAnswer")
-const resultCard = document.getElementById("resultCard")
-const resultCard2 = document.getElementById("resultCard2")
-const feedback1 = document.getElementById("feedback1")
-const feedback2 = document.getElementById("feedback2")
-const questionStatement = document.getElementById("questionStatement")
-const badResult = document.getElementById("badResult")
-const goodResult = document.getElementById("goodResult")
+const answerComprobation = document.getElementById("answerComprobation");
+const incorrectAnswer = document.getElementById("incorrectAnswer");
+const correctAnswer = document.getElementById("correctAnswer");
+const resultCard = document.getElementById("resultCard");
+const resultCard2 = document.getElementById("resultCard2");
+const feedback1 = document.getElementById("feedback1");
+const feedback2 = document.getElementById("feedback2");
+const questionStatement = document.getElementById("questionStatement");
+const badResult = document.getElementById("badResult");
+const goodResult = document.getElementById("goodResult");
+const congrats = document.getElementById("congrats");
+const almost = document.getElementById("almost");
+const deception = document.getElementById("deception");
 let currentQuestionIndex;
-let correctAnswersArr = []
-let result = 0
+let questions = [];
+let correctAnswersArr = [];
+let result = 0;
+
 const goHome = () => {
   results.classList.add("hide");
   questionContainerElement.classList.add("hide");
@@ -30,7 +35,7 @@ const goHome = () => {
   home.style = "display:flex;";
   home.classList.remove("hide");
   startButton.classList.remove("hide");
-  resultCard.classList.add("d-none")
+  resultCard.classList.add("d-none");
 };
 const goResults = () => {
   home.classList.add("hide");
@@ -41,9 +46,9 @@ const goResults = () => {
   startButton.classList.add("hide");
   results.classList.remove("hide");
   resultCard.classList.remove("hide");
-  resultCard.classList.add("d-none")
-  showResults()
-  printResult()
+  resultCard.classList.add("d-none");
+  showResults();
+  printResult();
 };
 const showResults = () => {
   home.classList.add("hide");
@@ -53,10 +58,9 @@ const showResults = () => {
   questionContainerElement.classList.add("hide");
   startButton.classList.add("hide");
   results.classList.remove("hide");
-  resultsContainer.classList.remove("hide")
-  resultCard.classList.replace("d-none", "d-flex")
-}
-
+  resultsContainer.classList.remove("hide");
+  resultCard.classList.replace("d-none", "d-flex");
+};
 const startGame = () => {
   resetState();
   startButton.classList.add("hide");
@@ -66,60 +70,60 @@ const startGame = () => {
   setNextQuestion();
 };
 
-let questions = [];
 axios
   .get(
     "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
   )
   .then((res) => (questions = res.data.results))
   .catch((err) => console.error(err));
+
 const showQuestion = (question) => {
   questionElement.innerHTML = question.question;
-  questionStatement.innerHTML = `Question ${currentQuestionIndex + 1}:`
-  const answers = [];
-  answers.push({ text: question.correct_answer, correct: true });
-  question.incorrect_answers.forEach((element) => {
-    answers.push({ text: element, correct: false });
-    answers.sort(function () {
-      return Math.random() - 0.5;
-    });
+  questionStatement.innerHTML = `Question ${currentQuestionIndex + 1}:`;
+
+const answers = [];
+answers.push({ text: question.correct_answer, correct: true });
+question.incorrect_answers.forEach((element) => {
+  answers.push({ text: element, correct: false });
+  answers.sort(function () {
+    return Math.random() - 0.5;
   });
-  console.log(answers);
-  answers.forEach((answer) => {
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-outline-light");
-    button.innerHTML = answer.text;
-    if (answer.correct === true) {
-      button.dataset.correct = true;
+});
+console.log(answers);
+answers.forEach((answer) => {
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-outline-light");
+  button.innerHTML = answer.text;
+  if (answer.correct === true) {
+    button.dataset.correct = true;
+  }
+  nextButton.classList.add("disabled");
+
+  button.addEventListener("click", (e) => {
+    if (e.target.dataset.correct === "true") {
+      answerComprobation.classList.remove("hide");
+      answerComprobation.innerHTML = "Excelent!";
+      result++;
+      correctAnswer.play();
+    } else {
+      answerComprobation.classList.remove("hide");
+      answerComprobation.innerHTML = "You choose the wrong answer!";
+      incorrectAnswer.play();
     }
-    button.addEventListener("click", (e) => {
-      if (e.target.dataset.correct === "true") {
-        answerComprobation.classList.remove("hide")
-        answerComprobation.innerHTML = "Excelent!"
-        result++// las anteriores dos lineas traté de mostrar las respuestas correctas, resultado undefined y muestra las 10 respuestas
-        correctAnswer.play()
-      } else {
-        answerComprobation.classList.remove("hide")
-        answerComprobation.innerHTML = "You choose the wrong answer!"
-        incorrectAnswer.play()
-      }
-      selectAnswer();
-    });
-    answerButtons.appendChild(button);
+    selectAnswer();
   });
-};
+  answerButtons.appendChild(button);
+})};
 
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
-  if (currentQuestionIndex === 9) {
-      setTimeout(goResults, 5000)
-    }
 });
 
 const setNextQuestion = () => {
   resetState();
-  answerComprobation.classList.add("hide")
+  answerComprobation.classList.add("hide");
+
   showQuestion(questions[currentQuestionIndex]);
 };
 
@@ -137,29 +141,38 @@ const selectAnswer = () => {
   Array.from(answerButtons.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct === "true");
   });
-  if (questions.length > currentQuestionIndex + 1) {
-    nextButton.classList.replace("d-none", "d-flex");
+  if (currentQuestionIndex === 9) {
+    nextButton.classList.add("d-none");
+    setTimeout(goResults, 3500);
+  } else if (questions.length > currentQuestionIndex + 1) {
+    nextButton.classList.add("d-flex");
+    nextButton.classList.remove("disabled");
   } else {
     startButton.classList.add("hide");
     cardContainer.classList.add("d-none");
   }
 };
 const printResult = () => {
-  showResults()
-  feedback1.innerHTML = `Correct answers: ${result}`
-  if (result >= 5) {
-    resultCard2.classList.replace("bg-dark", "bg-success")
-    feedback2.innerHTML =  "Excelent!!!! Good job!"
-    goodResult.play()
+  showResults();
+  feedback1.innerHTML = `Your score is ${result}/10`;
+  if (result >= 7) {
+    resultCard2.classList.replace("bg-dark", "bg-success");
+    congrats.classList.remove("hide");
+    feedback2.innerHTML = "Excelent!!! Good job!";
+    goodResult.play();
+  } else if (result >= 4 && result <= 6) {
+    resultCard2.classList.replace("bg-dark", "bg-secondary");
+    almost.classList.remove("hide");
+    feedback2.innerHTML = "Come on! You could do it better!";
   } else {
-    resultCard2.classList.replace("bg-dark", "bg-danger")
-    feedback2.innerHTML =  "Sorry, you need to work harder"
-    badResult.play()
+    deception.classList.remove("hide");
+    resultCard2.classList.replace("bg-dark", "bg-danger");
+    feedback2.innerHTML = "Sorry, you need to work harder.";
+    badResult.play();
   }
-}
+};
 
 const resetState = () => {
-  nextButton.classList.add("d-none");
   answerButtons.innerHTML = "";
 };
 
